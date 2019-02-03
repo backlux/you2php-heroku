1a81b9cb4a6dc18319b6aa6ca884b7e8424d9cf5
backlux <mantax@126.com>
2019-02-03 20:29:44 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,7 +1,7 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
+define('APIKEY', 'AIzaSyD4MEAZka4CMN80MP4Jzld_23bxzGBKE-I');//这里改成您的api key
+define('GJ_CODE', 'US');//国家代码
 define('SITE_NAME', 'Youtube');//网站名字
 define('TITLENAME', 'Youtube');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
